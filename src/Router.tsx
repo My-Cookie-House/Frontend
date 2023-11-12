dc2eb8816bca183822fa324561c133f81ac4eead
minseong0324 <kwan0324@naver.com>
2023-11-13 03:49:28 +0900

    feat: 추가 셋팅

---
+++ src/Router.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  );
+}
