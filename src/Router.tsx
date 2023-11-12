5d8269a4038fabf2413ce61c0d4301ebc5b00044
minseong0324 <kwan0324@naver.com>
2023-11-13 04:31:46 +0900

    feat: Router 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GuestBook from "./pages/GuestBook/GuestBook";
+import Mission from "./pages/Mission/Mission";
 
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        
+        <Route path="/:id/guests" element={<GuestBook />} />
+        <Route path="/mission" element={<Mission />} />
       </Routes>
     </BrowserRouter>
   );
