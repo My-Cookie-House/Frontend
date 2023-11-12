bb8e0ce0ec1147b8faf8a130f64babce6f56b9d4
minseong0324 <kwan0324@naver.com>
2023-11-13 04:30:22 +0900

    feat: GlobalStyle, 폰트 추가

---
+++ src/style.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+import Pretendard from "./static/font/Pretendard-Medium.woff2";
+
+export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Pretendard';
+    src: url(${Pretendard}) format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  html {
+    font-size: 16px; // 16px 기본 설정
+
+  }
+
+  body, html {
+    font-family: 'Pretendard';
+    font-weight: 700;
+    margin: 0;
+    font-size: 16px;
+  }
+
+  #content {
+    position: relative;
+    height: 100%;
+    overflow: auto;
+    z-index: 1;
+  }
+`;
