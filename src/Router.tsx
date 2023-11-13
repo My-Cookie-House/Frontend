8dfa9b9ab2e201bfd6786a677e657f81bfb3e08a
usernamebuzz <ohminji@ewhain.net>
2023-11-13 20:34:57 +0900

    Fix: GuestBook 오류 수정, Login 페이지 만들기

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,14 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import React from 'react';
 import GuestBook from './pages/GuestBook/GuestBook';
 import Mission from './pages/Mission/Mission';
 import OnboardingPage from './pages/OnboardingPage/OnboardingPage';
+import Login from './pages/Login/Login';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<OnboardingPage />} />
         <Route path="/:id/guests" element={<GuestBook />} />
         <Route path="/mission" element={<Mission />} />
