891b5d87984cb7f28d6e62df6a001bcdbc1d60e9
usernamebuzz <ohminji@ewhain.net>
2023-11-14 08:22:51 +0900

    Chore: Rename

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import GuestBook from './pages/GuestBook/GuestBook';
 import Mission from './pages/Mission/Mission';
-import OnboardingPage from './pages/OnboardingPage/OnboardingPage';
+import Onboarding from './pages/Onboarding/Onboarding';
 import Login from './pages/Login/Login';
 import Build from './pages/Build/Build';
 import Random from './pages/Build/Random/Random';
@@ -12,7 +12,7 @@ export default function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<OnboardingPage />} />
+        <Route path="/" element={<Onboarding />} />
         <Route path="/:id/guests" element={<GuestBook />} />
         <Route path="/mission" element={<Mission />} />
         <Route path="/build" element={<Build />} />
