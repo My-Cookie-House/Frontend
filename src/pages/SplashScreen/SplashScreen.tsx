c05e11141ddd96a860fbf1c04971a590903ffe9e
usernamebuzz <ohminji@ewhain.net>
2023-11-14 13:48:50 +0900

    Feat: 0.5초 뒤에 Splash Screen에서 Login Page로

--- src/pages/SplashScreen/SplashScreen.tsx
+++ src/pages/SplashScreen/SplashScreen.tsx
@@ -1,5 +1,21 @@
+import {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
 import PageLayout from '../../components/PageLayout/PageLayout';
 
-export default function Mission() {
-  return <PageLayout>Splash</PageLayout>;
+export default function SplashScreen(): JSX.Element {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/login');
+    }, 500);
+
+    return () => clearTimeout(timer); // 컴포넌트가 언마운트되면 타이머를 취소
+  }, [navigate]);
+
+  return (
+    <PageLayout>
+      <div>splash</div>
+    </PageLayout>
+  );
 }
