5f73f4b991adc03c81153e4d8db58c41100c9123
minseong0324 <kwan0324@naver.com>
2023-11-15 08:58:37 +0900

    Fix: 함수 전달 추가

--- src/pages/House/House.tsx
+++ src/pages/House/House.tsx
@@ -22,11 +22,16 @@ export default function House() {
     gcTime: 1000 * 60 * STALE_MIN,
   });
 
+   // Mission 모달을 여는 함수
+   const handleOpenMissionModal = () => {
+    // TODO: 이 쓸데없는 함수 없이 어떻게 깔끔하게 정리할 수 있을까..
+  };
+
   // 방문한 쿠키하우스의 아이디와 현재 로그인한 유저의 아이디가 같은 경우만 미션 버튼 노출!
   return (
     <PageLayout
       guestBook={`/${id}/guests`}
-      mission={userId === +id}
+      mission={userId === +id ? handleOpenMissionModal : undefined} // 조건부로 함수 전달
       goBack={pathname === `/${id}/inside` && `/${id}`} // 하우스 내부에서만 뒤로가기 버튼 존재
     >
       <S.HouseName>{data?.houseName}</S.HouseName>
