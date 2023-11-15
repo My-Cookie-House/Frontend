02505a654bcd317cb59f168c7dbffdd052360624
usernamebuzz <ohminji@ewhain.net>
2023-11-15 16:03:49 +0900

    Design: 요소들 위치 지정

--- src/components/OnboardingComponents/style.tsx
+++ src/components/OnboardingComponents/style.tsx
@@ -2,6 +2,8 @@ import {styled} from 'styled-components';
 import theme from '../../theme';
 
 const Title = styled.h1`
+  margin-top: 48.3px;
+  margin-bottom: 39px;
   color: #f9f1e1;
   text-align: center;
   font-family: Pretendard;
@@ -19,6 +21,7 @@ const BottomText = styled.h2`
   font-style: normal;
   font-weight: 600;
   line-height: normal;
+  margin-bottom: 15.47px;
 `;
 
 const ExtraText = styled.h3`
