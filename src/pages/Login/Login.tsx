42008086326fc6463f7797187782ab70e3d0fcdb
usernamebuzz <ohminji@ewhain.net>
2023-11-16 20:55:15 +0900

    Fix: QueryClientProvider추가

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,3 +1,4 @@
+import {QueryClientProvider, QueryClient} from 'react-query';
 import PageLayout from '../../components/PageLayout/PageLayout';
 import LoginText from '../../assets/LoginAssets/LoginText.svg';
 import GoogleLogin from '../../assets/LoginAssets/GoogleLogin.svg';
@@ -7,30 +8,34 @@ import useLogin from '../../hooks/useLogin';
 import {useRecoilState} from 'recoil';
 import {loginMethodAtom} from '../../atoms/loginAtom';
 
+const queryClient = new QueryClient();
+
 export default function Login() {
   const [loginMethod, setLoginMethod] = useRecoilState(loginMethodAtom);
 
-  const handleKakaoLogin = () => {
+  function handleKakaoLogin() {
     setLoginMethod('kakao');
     useLogin();
-  };
+  }
 
-  const handleGoogleLogin = () => {
+  function handleGoogleLogin() {
     setLoginMethod('google');
     useLogin();
-  };
+  }
 
   return (
-    <PageLayout>
-      <img src={LoginText} />
-      <img src={whiteTree} />
-      <img
-        src={KakaoLogin}
-        alt="KakaoLogin"
-        style={{width: '240.845px', height: '53.239px'}}
-        onClick={handleKakaoLogin}
-      />
-      <img src={GoogleLogin} onClick={handleGoogleLogin} />
-    </PageLayout>
+    <QueryClientProvider client={queryClient}>
+      <PageLayout>
+        <img src={LoginText} alt="Login" />
+        <img src={whiteTree} alt="White Tree" />
+        <img
+          src={KakaoLogin}
+          alt="KakaoLogin"
+          style={{width: '240.845px', height: '53.239px'}}
+          onClick={handleKakaoLogin}
+        />
+        <img src={GoogleLogin} alt="GoogleLogin" onClick={handleGoogleLogin} />
+      </PageLayout>
+    </QueryClientProvider>
   );
 }
