a25d5208439fdf72449fa8b69d8bd7908a21fff2
usernamebuzz <ohminji@ewhain.net>
2023-11-17 00:54:00 +0900

    Feat: scope=profile_nickname 받기

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -10,13 +10,14 @@ const queryClient = new QueryClient();
 const Login = () => {
   const KAKAO_REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
   const KAKAO_REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&scope=profile_nickname`;
 
   return (
     <QueryClientProvider client={queryClient}>
       <PageLayout>
         <img src={LoginText} alt="Login" />
         <img src={whiteTree} alt="White Tree" />
+        <p>{KAKAO_REST_API_KEY}</p>
         <p>{KAKAO_REDIRECT_URI}</p>
         <a href={KAKAO_AUTH_URL}>
           <img
