ffc859e36221865872c512b7c8e0776c74e35cfa
usernamebuzz <ohminji@ewhain.net>
2023-11-17 15:18:25 +0900

    Chore: baseURL업데이트

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL:
-    'http://ec2-13-125-210-236.ap-northeast-2.compute.amazonaws.com:8080',
+  baseURL: 'ec2-3-35-218-95.ap-northeast-2.compute.amazonaws.com:8080',
 });
