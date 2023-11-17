7e1b0a1f96e60187110684cfc02c8fdfb2cda13c
minseong0324 <kwan0324@naver.com>
2023-11-18 00:37:43 +0900

    Fix : api 분리

---
+++ src/apis/guestBook.ts
@@ -0,0 +1,25 @@
+import { instance } from './axios';
+
+// 사용자의 방명록 정보를 가져오는 함수
+export const getUserInfoFromServer = async (userId) => {
+  try {
+    const response = await instance.get(`/guest-book/${userId}`);
+    return response.data.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// 편지를 보내는 함수
+export const sendGuestBook = async (userId, author, ornamentId, content) => {
+  try {
+    await instance.post('/guest-book', {
+        userId : userId,
+        author : author,
+        ornamentId : ornamentId,
+        content : content,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
