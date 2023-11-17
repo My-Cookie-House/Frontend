70ef0a5f2d95ff1549578f12cd9a9dbea106db7f
minseong0324 <kwan0324@naver.com>
2023-11-18 05:26:02 +0900

    feat : FurnitureLayer png 모두 추가, import하는 component 작성

---
+++ src/assets/FurnitureLayer/index.ts
@@ -0,0 +1,153 @@
+import FurnitureLayer11 from './1-1.png';
+import FurnitureLayer12 from './1-2.png';
+import FurnitureLayer13 from './1-3.png';
+import FurnitureLayer21 from './2-1.png';
+import FurnitureLayer22 from './2-2.png';
+import FurnitureLayer23 from './2-3.png';
+import FurnitureLayer31 from './3-1.png';
+import FurnitureLayer32 from './3-2.png';
+import FurnitureLayer33 from './3-3.png';
+import FurnitureLayer41 from './4-1.png';
+import FurnitureLayer42 from './4-2.png';
+import FurnitureLayer43 from './4-3.png';
+import FurnitureLayer51 from './5-1.png';
+import FurnitureLayer52 from './5-2.png';
+import FurnitureLayer53 from './5-3.png';
+import FurnitureLayer61 from './6-1.png';
+import FurnitureLayer62 from './6-2.png';
+import FurnitureLayer63 from './6-3.png';
+import FurnitureLayer71 from './7-1.png';
+import FurnitureLayer72 from './7-2.png';
+import FurnitureLayer73 from './7-3.png';
+import FurnitureLayer81 from './8-1.png';
+import FurnitureLayer82 from './8-2.png';
+import FurnitureLayer83 from './8-3.png';
+import FurnitureLayer91 from './9-1.png';
+import FurnitureLayer92 from './9-2.png';
+import FurnitureLayer93 from './9-3.png';
+import FurnitureLayer101 from './10-1.png';
+import FurnitureLayer102 from './10-2.png';
+import FurnitureLayer103 from './10-3.png';
+import FurnitureLayer111 from './11-1.png';
+import FurnitureLayer112 from './11-2.png';
+import FurnitureLayer113 from './11-3.png';
+import FurnitureLayer121 from './12-1.png';
+import FurnitureLayer122 from './12-2.png';
+import FurnitureLayer123 from './12-3.png';
+import FurnitureLayer131 from './13-1.png';
+import FurnitureLayer132 from './13-2.png';
+import FurnitureLayer133 from './13-3.png';
+import FurnitureLayer141 from './14-1.png';
+import FurnitureLayer142 from './14-2.png';
+import FurnitureLayer143 from './14-3.png';
+import FurnitureLayer151 from './15-1.png';
+import FurnitureLayer152 from './15-2.png';
+import FurnitureLayer153 from './15-3.png';
+import FurnitureLayer161 from './16-1.png';
+import FurnitureLayer162 from './16-2.png';
+import FurnitureLayer163 from './16-3.png';
+import FurnitureLayer171 from './17-1.png';
+import FurnitureLayer172 from './17-2.png';
+import FurnitureLayer173 from './17-3.png';
+import FurnitureLayer181 from './18-1.png';
+import FurnitureLayer182 from './18-2.png';
+import FurnitureLayer183 from './18-3.png';
+import FurnitureLayer191 from './19-1.png';
+import FurnitureLayer192 from './19-2.png';
+import FurnitureLayer193 from './19-3.png';
+import FurnitureLayer201 from './20-1.png';
+import FurnitureLayer202 from './20-2.png';
+import FurnitureLayer203 from './20-3.png';
+import FurnitureLayer211 from './21-1.png';
+import FurnitureLayer212 from './21-2.png';
+import FurnitureLayer213 from './21-3.png';
+import FurnitureLayer221 from './22-1.png';
+import FurnitureLayer222 from './22-2.png';
+import FurnitureLayer223 from './22-3.png';
+import FurnitureLayer231 from './23-1.png';
+import FurnitureLayer232 from './23-2.png';
+import FurnitureLayer233 from './23-3.png';
+import FurnitureLayer241 from './24-1.png';
+import FurnitureLayer242 from './24-2.png';
+import FurnitureLayer243 from './24-3.png';
+import FurnitureLayer251 from './25-1.png';
+import FurnitureLayer252 from './25-2.png';
+import FurnitureLayer253 from './25-3.png';
+
+export default [
+  FurnitureLayer11,
+  FurnitureLayer12,
+  FurnitureLayer13,
+  FurnitureLayer21,
+  FurnitureLayer22,
+  FurnitureLayer23,
+  FurnitureLayer31,
+  FurnitureLayer32,
+  FurnitureLayer33,
+  FurnitureLayer41,
+  FurnitureLayer42,
+  FurnitureLayer43,
+  FurnitureLayer51,
+  FurnitureLayer52,
+  FurnitureLayer53,
+  FurnitureLayer61,
+  FurnitureLayer62,
+  FurnitureLayer63,
+  FurnitureLayer71,
+  FurnitureLayer72,
+  FurnitureLayer73,
+  FurnitureLayer81,
+  FurnitureLayer82,
+  FurnitureLayer83,
+  FurnitureLayer91,
+  FurnitureLayer92,
+  FurnitureLayer93,
+  FurnitureLayer101,
+  FurnitureLayer102,
+  FurnitureLayer103,
+  FurnitureLayer111,
+  FurnitureLayer112,
+  FurnitureLayer113,
+  FurnitureLayer121,
+  FurnitureLayer122,
+  FurnitureLayer123,
+  FurnitureLayer131,
+  FurnitureLayer132,
+  FurnitureLayer133,
+  FurnitureLayer141,
+  FurnitureLayer142,
+  FurnitureLayer143,
+  FurnitureLayer151,
+  FurnitureLayer152,
+  FurnitureLayer153,
+  FurnitureLayer161,
+  FurnitureLayer162,
+  FurnitureLayer163,
+  FurnitureLayer171,
+  FurnitureLayer172,
+  FurnitureLayer173,
+  FurnitureLayer181,
+  FurnitureLayer182,
+  FurnitureLayer183,
+  FurnitureLayer191,
+  FurnitureLayer192,
+  FurnitureLayer193,
+  FurnitureLayer201,
+  FurnitureLayer202,
+  FurnitureLayer203,
+  FurnitureLayer211,
+  FurnitureLayer212,
+  FurnitureLayer213,
+  FurnitureLayer221,
+  FurnitureLayer222,
+  FurnitureLayer223,
+  FurnitureLayer231,
+  FurnitureLayer232,
+  FurnitureLayer233,
+  FurnitureLayer241,
+  FurnitureLayer242,
+  FurnitureLayer243,
+  FurnitureLayer251,
+  FurnitureLayer252,
+  FurnitureLayer253
+];
