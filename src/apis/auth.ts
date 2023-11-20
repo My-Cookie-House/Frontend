4c386ae9839beccd167115bb68f9ddacb56dcdb9
stakbucks <dhassidu11@gmail.com>
2023-11-21 00:20:24 +0900

    Fix: Cookie 오타 Cookies로 수정

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,9 +1,9 @@
-import Cookie from 'js-cookie';
+import Cookies from 'js-cookie';
 export async function getLoginUserInfo() {
   try {
     const response: any = await instance.get('/user', {
       headers: {
-        Authorization: Cookie.get('accessToken'),
+        Authorization: Cookies.get('accessToken'),
       },
     });
     return response.data.data;
@@ -15,7 +15,6 @@ export async function getLoginUserInfo() {
 import axios from 'axios';
 import {Data} from '../atoms/loginAtom';
 import {instance} from './axios';
-import Cookies from '@/pages/Build/Custom/Cookies/Cookies';
 
 export default async function tryLogin(
   code: string,
