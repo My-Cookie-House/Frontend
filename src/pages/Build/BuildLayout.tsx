5778e49aa36e2396c446124318021d5f0c4b09fd
stakbucks <dhassidu11@gmail.com>
2023-11-22 21:27:51 +0900

    Fix: 주석 처리 해제

--- src/pages/Build/BuildLayout.tsx
+++ src/pages/Build/BuildLayout.tsx
@@ -27,14 +27,14 @@ export default function BuildLayout() {
      * 유저 정보 확인해서, 빌드를 한 적 있으면 접속 못하게 막기!
      * (집을 다시 만드는 것을 방지)
      */
-    // if (!loggedIn) {
-    //   alert('로그인 해주세요');
-    //   navigate('/login');
-    // }
-    // if (isHouseBuilt) {
-    //   alert('이미 입주하신 쿠키하우스가 존재합니다!');
-    //   navigate(`/${userId}`);
-    // }
+    if (!loggedIn) {
+      alert('로그인 해주세요');
+      navigate('/login');
+    }
+    if (isHouseBuilt) {
+      alert('이미 입주하신 쿠키하우스가 존재합니다!');
+      navigate(`/${userId}`);
+    }
   }, []);
 
   useEffect(() => {
