a8bf8c8d8efde0ca972ea61b6af1258cc1de64ce
usernamebuzz <ohminji@ewhain.net>
2023-11-23 17:45:35 +0900

    Bug: 새로고침해야만 '외출하기', '철거하기' 보임

--- src/pages/House/Outside/Outside.tsx
+++ src/pages/House/Outside/Outside.tsx
@@ -19,6 +19,9 @@ const GC_MIN = 5;
 
 export default function Outside() {
   const {id, isMyHouse} = useIsMyHouse();
+  useEffect(() => {
+    console.log('isMyHouse value: ', isMyHouse);
+  }, [isMyHouse]);
 
   const {data} = useSuspenseQuery<IHouseOutside>({
     queryKey: ['house', 'outside', id],
@@ -77,38 +80,38 @@ export default function Outside() {
           {isMyHouse ? '집 안으로 들어가기' : '방문하기'}
         </NextStepText>
       </LongButton>
-      {isMyHouse ? (
-        <div>
+      {isMyHouse && (
+        <>
           <button onClick={() => setlogoutModal(true)}>외출하기</button>
           <button onClick={() => setSignoutModal(true)}>철거하기</button>
-          <GoOutModal
-            isOpen={logoutModal}
-            closeModal={closeLogout}
-            modalTitle={'외출하기'}
-            modalTexts={[
-              '외출하기를 누르시면',
-              '로그아웃이 됩니다.',
-              '로그아웃 하시겠습니까?',
-            ]}
-            yesBtnText={'예'}
-            onYes={logout}
-          />
-          <GoOutModal
-            isOpen={signoutModal}
-            closeModal={closeSignout}
-            modalTitle={'철거하기'}
-            modalTexts={[
-              '철거하기를 누르시면',
-              '회원 탈퇴가 됩니다.',
-              '모든 데이터가 삭제되고',
-              '새로운 집을 만들 수 있습니다',
-            ]}
-            yesBtnText={'탈퇴하기'}
-            onYes={signout}
-          />
-        </div>
-      ) : (
-        ''
+          <div>
+            <GoOutModal
+              isOpen={logoutModal}
+              closeModal={closeLogout}
+              modalTitle={'외출하기'}
+              modalTexts={[
+                '외출하기를 누르시면',
+                '로그아웃이 됩니다.',
+                '로그아웃 하시겠습니까?',
+              ]}
+              yesBtnText={'예'}
+              onYes={logout}
+            />
+            <GoOutModal
+              isOpen={signoutModal}
+              closeModal={closeSignout}
+              modalTitle={'철거하기'}
+              modalTexts={[
+                '철거하기를 누르시면',
+                '회원 탈퇴가 됩니다.',
+                '모든 데이터가 삭제되고',
+                '새로운 집을 만들 수 있습니다',
+              ]}
+              yesBtnText={'탈퇴하기'}
+              onYes={signout}
+            />
+          </div>
+        </>
       )}
     </>
   );
