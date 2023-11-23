66e456562413ebc8b24934e9bda85105f283346c
usernamebuzz <ohminji@ewhain.net>
2023-11-23 16:45:22 +0900

    Refactor: GoOutModal 재사용 가능

--- src/components/Modal/GoOutModal/GoOutModal.tsx
+++ src/components/Modal/GoOutModal/GoOutModal.tsx
@@ -6,25 +6,36 @@ import GingerMan from '@/assets/Cookies/GingerMan.svg';
 type Props = {
   closeModal: () => void;
   isOpen: boolean;
+  modalTitle: string;
+  modalTexts: string[];
+  yesBtnText: string;
+  onYes: () => void;
 };
 
-export default function GoOutModal({isOpen, closeModal}: Props) {
+export default function CustomModal({
+  isOpen,
+  closeModal,
+  modalTitle,
+  modalTexts,
+  yesBtnText,
+  onYes,
+}: Props) {
   return (
     <Modal
       isOpen={isOpen}
       onClose={closeModal}
       imageType={'MediumModal'}
-      modalTitle={'외출하기'}
+      modalTitle={modalTitle}
     >
       <ModalCloseButton onClick={closeModal} />
 
       <S.Wrapper>
-        <S.ModalText>{'외출하기를 누르시면'}</S.ModalText>
-        <S.ModalText>{'로그아웃이 됩니다.'}</S.ModalText>
-        <S.ModalText>{'로그아웃 하시겠습니까?'}</S.ModalText>
+        {modalTexts.map((text, index) => (
+          <S.ModalText key={index}>{text}</S.ModalText>
+        ))}
         <S.GingerManImg src={GingerMan} />
         <S.YesNoBtnWrapper>
-          <S.YesNoBtn>예</S.YesNoBtn>
+          <S.YesNoBtn onClick={onYes}>{yesBtnText}</S.YesNoBtn>
           <S.YesNoBtn onClick={closeModal}>아니요</S.YesNoBtn>
         </S.YesNoBtnWrapper>
       </S.Wrapper>
