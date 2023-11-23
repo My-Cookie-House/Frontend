b5479ada2342c55af29d506ecf0869a98e4cf989
stakbucks <dhassidu11@gmail.com>
2023-11-23 15:38:04 +0900

    Feat: 서버 baseUrl 수정

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie';
 import useSetTokens from '../hooks/useSetTokens';
 
 export const instance = axios.create({
-  baseURL: 'https://cookie-house-api.site',
+  baseURL: 'https://cookie-house.store',
   headers: {
     Authorization: `${Cookies.get('accessToken')}`,
   },
