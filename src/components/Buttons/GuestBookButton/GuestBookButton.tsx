8b678a810efec5c79f20975ff62a6a57a6e0b8b0
stakbucks <dhassidu11@gmail.com>
2023-11-24 22:15:47 +0900

    Feat: GuestBook 버튼 생성

---
+++ src/components/Buttons/GuestBookButton/GuestBookButton.tsx
@@ -0,0 +1,14 @@
+import GuestBookImg from '@/assets/GuestBook/GuestBook.png';
+import * as S from './styled';
+
+type Props = {
+  onClick(): void;
+};
+
+export default function GuestBookButton({onClick}: Props) {
+  return (
+    <S.Button>
+      <S.GuestBookImg src={GuestBookImg} />
+    </S.Button>
+  );
+}
---
+++ src/components/Buttons/GuestBookButton/styled.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  width: 83px;
+  height: 83px;
+  border-radius: 41.5px;
+  border: 2px dashed #572e16;
+  background: #ba998b;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const GuestBookImg = styled.img`
+  width: 49.759px;
+  height: 59px;
+`;
