574a901aaeaf6587ca9cd2c3179b06e2875e00ec
usernamebuzz <ohminji@ewhain.net>
2023-11-24 10:05:29 +0900

    Chore: 토큰 지우기

--- src/components/AuthProvider/AuthProvider.tsx
+++ src/components/AuthProvider/AuthProvider.tsx
@@ -12,22 +12,11 @@ type Props = {
   children: React.ReactNode;
 };
 
-import {instance} from '@/apis/axios';
-import Cookies from 'js-cookie';
-
 export default function AuthProvider({children}: Props) {
   const setLoginState = useSetRecoilState(loginStateAtom);
   const setUserInfoState = useSetRecoilState(userInfoAtom);
   const userInfo = useRecoilValue(userInfoAtom);
-  instance.interceptors.request.use(function (config) {
-    config.headers.Authorization =
-      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImlhdCI6MTcwMDY1MDA3OCwiZXhwIjoxNzA5MjkwMDc4LCJ1c2VySWQiOiIxIn0.X-RWDTv319FZ4P-DoGHscNMXhuS6r-RHgVl7AC9SwKRbhawFnNAyEKF7fD7uIV8pxjf9T7LNRLOoJzfr6INtuQ';
-    return config;
-  });
-  Cookies.set(
-    'accessToken',
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0b2tlbiIsImlhdCI6MTcwMDY1MDA3OCwiZXhwIjoxNzA5MjkwMDc4LCJ1c2VySWQiOiIxIn0.X-RWDTv319FZ4P-DoGHscNMXhuS6r-RHgVl7AC9SwKRbhawFnNAyEKF7fD7uIV8pxjf9T7LNRLOoJzfr6INtuQ',
-  );
+
   const {data, isSuccess} = useSuspenseQuery<null | UserInfo>({
     queryKey: ['loginState'],
     queryFn: getLoginUserInfo,
