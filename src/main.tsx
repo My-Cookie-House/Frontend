bc8752afd9005444ec324b910a127a09f1fe686a
usernamebuzz <ohminji@ewhain.net>
2023-11-24 19:07:49 +0900

    Feat: QueryClientProviderMonitor사용

--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,29 @@ import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {ThemeProvider} from 'styled-components';
 import theme from './theme';
 import {RecoilRoot} from 'recoil';
+import {useEffect} from 'react';
+
+function QueryClientProviderMonitor({children}) {
+  useEffect(() => {
+    return () => {
+      console.log('쿼리클라이언트 프로바이더 언마운트');
+    };
+  }, []);
+
+  return children;
+}
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ThemeProvider theme={theme}>
     <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <GlobalStyle />
-        <App />
-      </RecoilRoot>
+      <QueryClientProviderMonitor>
+        <RecoilRoot>
+          <GlobalStyle />
+          <App />
+        </RecoilRoot>
+      </QueryClientProviderMonitor>
     </QueryClientProvider>
   </ThemeProvider>,
 );
