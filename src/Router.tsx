3e9413d34caf936c2e310f9fcd7bcf2aeb5f7efa
stakbucks <dhassidu11@gmail.com>
2023-11-25 22:53:26 +0900

    Feat: location 변경에 따른 send 호출

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,3 +1,6 @@
+import {useEffect, useState} from 'react';
+import {useLocation} from 'react-router-dom';
+import ReactGA from 'react-ga4';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import GuestBook from './pages/GuestBook/GuestBook';
 import Onboarding from './pages/Onboarding/Onboarding';
@@ -15,10 +18,35 @@ import House from './pages/House/House';
 import Outside from './pages/House/Outside/Outside';
 import Inside from './pages/House/Inside/Inside';
 import Redirect from './pages/Login/Redirect';
-import MissionFurniturePreview from './pages/MissionFurniturePreview/MissionFurniturePreview'
+import MissionFurniturePreview from './pages/MissionFurniturePreview/MissionFurniturePreview';
 import Wallpaper from './pages/Build/Custom/Wallpaper/Wallpaper';
 
 export default function Router() {
+  /* src/RouteChangeTracker.js */
+
+  /**
+   * uri 변경 추적 컴포넌트
+   * uri가 변경될 때마다 pageview 이벤트 전송
+   */
+  const location = useLocation();
+  const [initialized, setInitialized] = useState(false);
+
+  // 구글 애널리틱스 운영서버만 적용
+  useEffect(() => {
+    if (process.env.REACT_APP_GOOGLE_ANALYTICS) {
+      ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
+      setInitialized(true);
+    }
+  }, []);
+
+  // location 변경 감지시 pageview 이벤트 전송
+  useEffect(() => {
+    if (initialized) {
+      ReactGA.set({page: location.pathname});
+      ReactGA.send('pageview');
+    }
+  }, [initialized, location]);
+
   return (
     <BrowserRouter>
       <ScrollToTop />
@@ -33,7 +61,10 @@ export default function Router() {
         <Route path="/:id" element={<House />}>
           <Route path="" element={<Outside />} />
           <Route path="inside" element={<Inside />} />
-          <Route path="custom/furniture" element={<MissionFurniturePreview />} />
+          <Route
+            path="custom/furniture"
+            element={<MissionFurniturePreview />}
+          />
         </Route>
 
         {/* 쿠키하우스 빌딩 하위 경로 */}
--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,15 @@ import {ThemeProvider} from 'styled-components';
 import theme from './theme';
 import {RecoilRoot} from 'recoil';
 import {useEffect, useRef} from 'react';
+import ReactGA from 'react-ga4';
 
 function QueryClientProviderMonitor({children}) {
   const prevQueryClientRef = useRef<QueryClient | null>(null);
+
+  // 구글 애널리틱스 운영서버만 적용
+  if (process.env.REACT_APP_GOOGLE_ANALYTICS) {
+    ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
+  }
 
   useEffect(() => {
     if (prevQueryClientRef.current !== queryClient) {
