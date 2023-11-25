8a02372afedecf6bb4f6f8fd1fd8e82de5bc59f8
minseong0324 <kwan0324@naver.com>
2023-11-26 05:52:38 +0900

    Fix : 확장자

--- src/components/ImportOrnaments/ImportOrnaments.tsx
+++ src/components/ImportOrnaments/ImportOrnaments.tsx
@@ -1,16 +1,16 @@
 
-import Ornament1 from '../../assets/Ornament/Ornament1.svg';
-import Ornament2 from '../../assets/Ornament/Ornament2.svg';
-import Ornament3 from '../../assets/Ornament/Ornament3.svg';
-import Ornament4 from '../../assets/Ornament/Ornament4.svg';
-import Ornament5 from '../../assets/Ornament/Ornament5.svg';
-import Ornament6 from '../../assets/Ornament/Ornament6.svg';
-import Ornament7 from '../../assets/Ornament/Ornament7.svg';
-import Ornament8 from '../../assets/Ornament/Ornament8.svg';
-import Ornament9 from '../../assets/Ornament/Ornament9.svg';
-import Ornament10 from '../../assets/Ornament/Ornament10.svg';
-import Ornament11 from '../../assets/Ornament/Ornament11.svg';
-import Ornament12 from '../../assets/Ornament/Ornament12.svg';
+import Ornament1 from '../../assets/Ornament/Ornament1.webp';
+import Ornament2 from '../../assets/Ornament/Ornament2.webp';
+import Ornament3 from '../../assets/Ornament/Ornament3.webp';
+import Ornament4 from '../../assets/Ornament/Ornament4.webp';
+import Ornament5 from '../../assets/Ornament/Ornament5.webp';
+import Ornament6 from '../../assets/Ornament/Ornament6.webp';
+import Ornament7 from '../../assets/Ornament/Ornament7.webp';
+import Ornament8 from '../../assets/Ornament/Ornament8.webp';
+import Ornament9 from '../../assets/Ornament/Ornament9.webp';
+import Ornament10 from '../../assets/Ornament/Ornament10.webp';
+import Ornament11 from '../../assets/Ornament/Ornament11.webp';
+import Ornament12 from '../../assets/Ornament/Ornament12.webp';
 
 interface Ornament {
   id: number;
