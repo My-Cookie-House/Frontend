6c245b4213ec482fc950c81eaeba518af9ab6a85
usernamebuzz <ohminji@ewhain.net>
2023-11-28 14:47:22 +0900

    Feat: 지원하지 않는 이미지 파일 형식입니다

--- src/pages/MissionFurniturePreview/MissionFurniturePreview.tsx
+++ src/pages/MissionFurniturePreview/MissionFurniturePreview.tsx
@@ -92,7 +92,11 @@ export default function MissionFurniturePreview() {
       setUserInfo((prev) => ({...prev, todayMissionComplete: true}));
       navigate(`/${userId}/inside`);
     } catch (error) {
-      alert('업로드에 실패했어요.');
+      if ((error.response.message = '지원하지 않는 이미지 파일 형식입니다')) {
+        alert('지원하지 않는 이미지 파일 형식입니다');
+      } else {
+        alert('업로드에 실패했어요.');
+      }
     }
   };
 
